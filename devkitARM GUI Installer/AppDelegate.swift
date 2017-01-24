4367bb1ca42c27e3adf84a617717302843fdbad3
Adrian <adrian@Adrians-iMac.local>
2017-01-24 18:20:38 -0300

    Initial Commit

---
+++ devkitARM GUI Installer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  devkitARM GUI Installer
+//
+//  Created by Adrian on 24.01.17.
+//  Copyright © 2017 Adrian. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
